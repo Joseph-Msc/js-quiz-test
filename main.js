4d0744b3b0eaa1a4eccb718fe57f35eac8147cff
Avram Cosmin <info@sprindo.co.uk>
2020-07-31 18:07:33 +0300

    Some more debugging.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const divEl = document.getElementById('container');
 const request = getQuizContent('https://run.mocky.io/v3/7449edd2-32c6-4404-a77f-6b4d088f1cb0');
 request.onreadystatechange = (res) => {
   // we want to generate the dom node elements that will represent the html structure of the quiz content.
-  console.log(res);
+  console.log(res, request.response);
 }
 request.send();
 
