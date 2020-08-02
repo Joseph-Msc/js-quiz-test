f264ed99710843b213dc7850ad9365902e5c78ed
Joseph Wong <wongj105.319@gmail.com>
2020-08-02 13:56:53 +0100

    homework 02.08.2020

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const divEl = document.getElementById('container');
-const request = getQuizContent('https://run.mocky.io/v3/7449edd2-32c6-4404-a77f-6b4d088f1cb0');
-request.onreadystatechange = (res) => {
-  // we want to generate the dom node elements that will represent the html structure of the quiz content.
-  console.log(res, request.response);
+function load(callback){
+  const request = getQuizContent('https://run.mocky.io/v3/7449edd2-32c6-4404-a77f-6b4d088f1cb0');
+  request.onreadystatechange = (res) => {
+    // we want to generate the dom node elements that will represent the html structure of the quiz content.
+    callback(request.response);
+  }
+  request.send();
 }
-request.send();
+
 
 function getQuizContent(url) {
   const http = new XMLHttpRequest();
@@ -12,3 +15,33 @@ function getQuizContent(url) {
 
   return http;
 }
+
+load((response) => {
+  const ulEl = document.createElement('ul');
+  console.log(response);
+  JSON.parse(response).data.forEach(
+    (quizObj) => {
+      const liEl = document.createElement('li');
+      const questionTextNode = document.createTextNode(quizObj.question);
+      liEl.appendChild(questionTextNode);
+      getAnwers(quizObj, liEl);
+    }
+  )
+  ulEl.appendChild(liEl);
+  divEl.appendChild(ulEl);
+}
+);
+
+function getAnswers(quizObj, liEl){
+  const ulEl = document.createElement('ul');
+  quizObj.answers.forEach(
+    (answerObj) => {
+      const liElAnswer = document.createElement('li');
+      const liElTextNode = document.createTextNode(answerObj.answer);
+      liElAnswer.appendChild(liElTextNode);
+    }
+  );
+  ulEl.appendChild(liElAnswer);
+  liEl.appendChild(ulEl);
+}
+
