a0e0bcaad37bb753a9c934d65e5da06364008b80
Avram Cosmin <info@sprindo.co.uk>
2020-08-02 18:49:43 +0300

    Fixed but you fix it

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
 const divEl = document.getElementById('container');
-function load(callback){
-  const request = getQuizContent('https://run.mocky.io/v3/7449edd2-32c6-4404-a77f-6b4d088f1cb0');
-  request.onreadystatechange = (res) => {
-    // we want to generate the dom node elements that will represent the html structure of the quiz content.
-    callback(request.response);
-  }
-  request.send();
+
+function load(callback) {
+    const request = getQuizContent('https://run.mocky.io/v3/632e1c5d-fdd4-4e10-a253-803e313bf91a');
+    request.onreadystatechange = (res) => {
+        // we want to generate the dom node elements that will represent the html structure of the quiz content.
+        callback(request.responseText);
+    }
+    request.send();
 }
 
 
 function getQuizContent(url) {
-  const http = new XMLHttpRequest();
-  http.open('GET', url, true);
+    const http = new XMLHttpRequest();
+    http.open('GET', url, true);
 
-  return http;
+    return http;
 }
 
 load((response) => {
-  const ulEl = document.createElement('ul');
-  console.log(response);
-  JSON.parse(response).data.forEach(
-    (quizObj) => {
-      const liEl = document.createElement('li');
-      const questionTextNode = document.createTextNode(quizObj.question);
-      liEl.appendChild(questionTextNode);
-      getAnwers(quizObj, liEl);
+        const ulEl = document.createElement('ul');
+        JSON.parse(response).data.forEach(
+            (quizObj) => {
+                const liEl = document.createElement('li');
+                const questionTextNode = document.createTextNode(quizObj.question);
+                liEl.appendChild(questionTextNode);
+                // getAnwers(quizObj, liEl);
+            }
+        )
+        //ulEl.appendChild(liEl);
+        divEl.appendChild(ulEl);
     }
-  )
-  ulEl.appendChild(liEl);
-  divEl.appendChild(ulEl);
-}
 );
 
-function getAnswers(quizObj, liEl){
-  const ulEl = document.createElement('ul');
-  quizObj.answers.forEach(
-    (answerObj) => {
-      const liElAnswer = document.createElement('li');
-      const liElTextNode = document.createTextNode(answerObj.answer);
-      liElAnswer.appendChild(liElTextNode);
-    }
-  );
-  ulEl.appendChild(liElAnswer);
-  liEl.appendChild(ulEl);
+function getAnswers(quizObj, liEl) {
+    const ulEl = document.createElement('ul');
+    quizObj.answers.forEach(
+        (answerObj) => {
+            const liElAnswer = document.createElement('li');
+            const liElTextNode = document.createTextNode(answerObj.answer);
+            liElAnswer.appendChild(liElTextNode);
+        }
+    );
+    ulEl.appendChild(liElAnswer);
+    liEl.appendChild(ulEl);
 }
 
